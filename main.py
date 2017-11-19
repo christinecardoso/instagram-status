fd02db85caf38a3928452455711bda3043228032
PMH <poriyamh42@gmail.com>
2017-11-19 19:40:56 +0330

    Add Web login UI
    User can now login to the instagram account using a web login form.
    a request with username and password information will be sent to the site and authenticates with InstagramAPI. Also one can Unfollow by sending the request to /unf

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 from flask import Flask, render_template, request
 from insta_login import InstaLogin
-from sys import exit
-
-username = ''
-password = ''
-
-if username and password:
-    insta = InstaLogin(username, password)
-else:
-    print('Please set username/password first!')
-    exit(1)
 
 app = Flask('instastatus')
-
-
+insta = False
 @app.route('/', methods=['GET', 'POST'])
-def main():
+def login():
+    global insta
     if request.method == 'GET':
-        flist = insta.get_followers()
-        felist = insta.get_followings()
-        selfpr = insta.get_self_profile()
-        return render_template('index.html',
-                               flist=flist,
-                               felist=felist,
-                               length=len(flist),
-                               felength=len(felist),
-                               username=username,
-                               profiledata=selfpr)
-    elif request.method == 'POST':
-        unf = request.form['unfollow']
-        insta.unfollow(unf)
-        return render_template('unfollow.html', unf=unf)
+        return render_template('login.html')
+    if request.method == 'POST':
+        if request.form['username']:
+            username = request.form['username']
+            password = request.form['password']
+            try:
+                if not insta:
+                    insta = InstaLogin(username, password)
+                flist = insta.get_followers()
+                felist = insta.get_followings()
+                selfpr = insta.get_self_profile()
+                return render_template('index.html',
+                                       flist=flist,
+                                       felist=felist,
+                                       length=len(flist),
+                                       felength=len(felist),
+                                       username=username,
+                                       profiledata=selfpr)
+            except:
+                return("Username or password is wrong!")
 
+@app.route('/unf', methods=['POST'])
+def unf():
+    unf = request.form['unfollow']
+    insta.unfollow(unf)
+    return render_template('unfollow.html', unf=unf)
 
 app.run(debug=True)
